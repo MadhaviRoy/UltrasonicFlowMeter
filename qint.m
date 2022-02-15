c12185dc91f40dbf905fc9faaa98427d8d95a6b8
Madhavi Chhugani <68702214+MadhaviRoy@users.noreply.github.com>
2022-02-16 03:50:16 +0545

    Update qint.m
    Updated Comment

--- qint.m
+++ qint.m
@@ -11,7 +11,7 @@
 % for a parabola fit through three points. 
 % Parabola is given by y(x) = a*(x-p)^2+b, 
 % where y(-1)=ym1, y(0)=y0, y(1)=yp1. 
-
+%p is the p_corr variable
 p = (yp1 - ym1)/(2*(2*y0 - yp1 - ym1)); 
 y = y0 - 0.25*(ym1-yp1)*p;
 a = 0.5*(ym1 - 2*y0 + yp1);
